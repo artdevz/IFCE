4b47fd5690c82593db241cdc23e7657198cfe3ac
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2023-10-01 20:40:25 -0300

    Update L3Q09.c

--- LabProg/Lista03/L3Q09.c
+++ LabProg/Lista03/L3Q09.c
@@ -1,8 +1,36 @@
 //L3-Q9:
 
 #include <stdio.h>
+#define TAM 3
 
 int main() {
+    int matriz[TAM][TAM];
     
+    // Criar Matriz:
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            int num;
+            printf("Digite o Valor da Linha x Coluna -> %d x %d: ", i+1, j+1);
+            scanf("%d", &num);
+            matriz[i][j] = num;
+        }    
+    }
+
+    // Exibir Matriz:
+    puts("");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%d  ", matriz[i][j]);
+        }
+        puts("\n");
+    }
+
+    // Exibir Diagonal Principal:
+    printf("Diagonal Principal -> ");
+    for (int i = 0; i < TAM; i++) {
+        int j = i;
+        printf(" %d ", matriz[i][j]);
+    }
+
     return 0;
 }
