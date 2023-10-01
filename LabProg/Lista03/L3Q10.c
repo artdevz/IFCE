7860ba96da7f9563a92ebb065ac8f23aae12aec3
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2023-10-01 20:41:32 -0300

    Update L3Q10.c

--- LabProg/Lista03/L3Q10.c
+++ LabProg/Lista03/L3Q10.c
@@ -1,8 +1,35 @@
 //L3-Q10:
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TAM 5
 
 int main() {
-    
+    srand(time(NULL));
+    int matriz[TAM][TAM];
+
+    // Gerar e Exibir Matriz:    
+    for (int i = 0; i < TAM; i++) {
+        printf("\n-> ");
+        for (int j = 0; j < TAM; j++) {
+            matriz[i][j] = rand() % 10;
+            printf("%d  ", matriz[i][j]);
+        }
+        puts("");
+    }
+
+    int x;
+    puts("\nDigite um Número para Pesquisar na Matriz: ");
+    scanf("%d", &x);
+
+    int count = 0;
+    for (int i = 0; i < TAM; i++) {        
+        for (int j = 0; j < TAM; j++) {
+            if (x == matriz[i][j]) count++;
+        }
+    }
+    printf("\nQuantidade de Números %d na Matriz -> %d\n", x, count);
+
     return 0;
 }
