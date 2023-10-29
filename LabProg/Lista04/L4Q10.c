52f90e8675a34981ec678152d9765a972bccda51
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2023-10-28 21:08:38 -0300

    Update L4Q10.c

--- LabProg/Lista04/L4Q10.c
+++ LabProg/Lista04/L4Q10.c
@@ -1,8 +1,34 @@
 //L4-Q10:
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
+    srand(time(NULL));
+
+    int *x = NULL, *f = NULL, tam, n;
     
+    puts("Digite o Tamanho do Vetor: ");
+    scanf("%d", &tam);
+    x = malloc(sizeof(int) * tam);
+    if (!x) exit(1);
+
+    puts("Digite o Intervalo dos Números do Vetor: ");
+    scanf("%d", &n);
+    f = malloc(sizeof(int) * n);
+    if (!n) exit(1);
+
+    for (int i = 0; i < tam; i++) {
+        int num = rand() % n;
+        *(x+i) = num;
+        printf("-> %d\n", *(x+i));
+        (*(f+num))++;            
+    }
+
+    for (int i = 0; i < n; i++) printf("\n%d -> [%d]", i, *(f+i));
+
+    free(x);
+    free(f);
     return 0;
 }
