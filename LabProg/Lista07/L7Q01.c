e8edc5ce6ec45169ed6b043da73b8de4ba0a22b1
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2023-12-13 17:58:54 -0300

    Update L7Q01.c

--- LabProg/Lista07/L7Q01.c
+++ LabProg/Lista07/L7Q01.c
@@ -1,1 +1,33 @@
 //L7-Q1:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXCHAR 30
+
+int main() {
+
+    char nome[MAXCHAR];
+    puts("Digite seu Primeiro Nome:");
+    fgets(nome, MAXCHAR, stdin);
+
+    int age;
+    puts("Digite sua Idade:");
+    scanf("%d", &age);
+
+    FILE *fp;
+    //char arq = "arquivo.csv";
+    fp = fopen("arq.csv", "w");
+    
+    if (!fp) {
+        puts("[ERRO] Não foi possível Abrir o Arquivo.");
+        exit(1);
+    }
+
+    fprintf(fp, "%s, %d", nome, age);
+
+    fclose(fp);
+
+    return 0;
+
+}
