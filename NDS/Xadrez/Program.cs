dbc8c1d1308b2e7d032276e14d6cbddf129b2a12
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-03 18:46:16 -0300

    .

--- NDS/Xadrez/Program.cs
+++ NDS/Xadrez/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Xadrez.pecas;
 
 namespace Xadrez;
 
@@ -7,7 +8,20 @@ class Program {
         
         Tabuleiro chess = new Tabuleiro();
         chess.gerarTabuleiro();
+        Peao pPreto1 = new Peao(false, true);
         chess.exibirTabuleiro();
+        pPreto1.MoverPeao(false, 0, 3);
+        chess.exibirTabuleiro(); 
+        pPreto1.MoverPeao(false, 0, 4);
+        chess.exibirTabuleiro();
+        pPreto1.MoverPeao(false, 0, 5);
+        chess.exibirTabuleiro(); 
+        pPreto1.MoverPeao(false, 0, 6);
+        chess.exibirTabuleiro();        
+        pPreto1.MoverPeao(false, 0, 7);
+        chess.exibirTabuleiro();
+        pPreto1.MoverPeao(false, 0, 8); // Erro de Index (Já esperado)
+        chess.exibirTabuleiro();  
 
     }
 }
