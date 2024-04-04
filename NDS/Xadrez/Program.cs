0b9a0c6179ce0b7f92c9b1fe9c3b5355d11a3e15
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-04 14:58:10 -0300

    Update Program.cs

--- NDS/Xadrez/Program.cs
+++ NDS/Xadrez/Program.cs
@@ -8,20 +8,38 @@ class Program {
         
         Tabuleiro chess = new Tabuleiro();
         chess.gerarTabuleiro();
-        Peao pPreto1 = new Peao(false, true);
-        chess.exibirTabuleiro();
-        pPreto1.MoverPeao(false, 0, 3);
-        chess.exibirTabuleiro(); 
-        pPreto1.MoverPeao(false, 0, 4);
-        chess.exibirTabuleiro();
-        pPreto1.MoverPeao(false, 0, 5);
-        chess.exibirTabuleiro(); 
-        pPreto1.MoverPeao(false, 0, 6);
-        chess.exibirTabuleiro();        
-        pPreto1.MoverPeao(false, 0, 7);
-        chess.exibirTabuleiro();
-        pPreto1.MoverPeao(false, 0, 8); // Erro de Index (Já esperado)
-        chess.exibirTabuleiro();  
+        
+        //IniciarJogo(){
+        List<Peao> peoes = new List<Peao>();
+        for (int i = 0; i < 16; i++) {
+            bool cor =  (i % 2 == 0)? true : false;
+            Peao peao = new Peao(cor, true);
+        }
+        
+        List<Bispo> bispos = new List<Bispo>();
+        List<Cavalo> cavalos = new List<Cavalo>();
+        List<Torre> torres = new List<Torre>();
+        
+        for (int i = 0; i < 4; i++) {
+            bool cor =  (i % 2 == 0)? true : false;
+            Bispo bispo = new Bispo(cor, true);
+            Cavalo cavalo = new Cavalo(cor, true);
+            Torre torre = new Torre(cor, true);
+        }
 
+        List<Rainha> rainhas = new List<Rainha>();
+        List<Rei> reis = new List<Rei>();
+        
+        for (int i = 0; i < 2; i++) {
+            bool cor = (i % 2 == 0)? true : false;
+            Rainha rainha = new Rainha(cor, true);
+            Rei rei = new Rei(cor, true);
+        }
+        //IniciarJogo()}
+        chess.exibirTabuleiro();
+    }
+
+    public static void IniciarJogo() {
+        // Ainda fazer
     }
 }
