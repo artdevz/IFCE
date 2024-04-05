66b4ad1ed3136acf7bac33b3d3a24fa87dac55d5
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-05 16:11:59 -0300

    Update Cavalo.cs

--- NDS/Xadrez/Pecas/Cavalo.cs
+++ NDS/Xadrez/Pecas/Cavalo.cs
@@ -19,16 +19,16 @@ class Cavalo {
             positionX = Cavalo.quantidadeBranco + 1 + Cavalo.quantidadePreto*4;
             positionY = 7;
             Cavalo.quantidadeBranco++;
-            Tabuleiro t = new Tabuleiro();
-            t.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, "\u265e");
+            //Tabuleiro t = new Tabuleiro();
+            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, "\u265e");
         }
         
         if ((isBranco == false) && (isInicio == true)) {
             positionX = Cavalo.quantidadePreto + 1 + Cavalo.quantidadePreto*4;
             positionY = 0;
             Cavalo.quantidadePreto++;
-            Tabuleiro t = new Tabuleiro();
-            t.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, "\u2658");
+            //Tabuleiro t = new Tabuleiro();
+            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, "\u2658");
         }
     }
 }
