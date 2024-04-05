1b421e657eada657c36385bbba12f5a349e69263
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-05 16:11:26 -0300

    Update Tabuleiro.cs

--- NDS/Xadrez/Tabuleiro.cs
+++ NDS/Xadrez/Tabuleiro.cs
@@ -7,22 +7,22 @@ class Tabuleiro {
     private static string[,] matrizDoTabuleiro = new string[8,8];
     
     // Métodos:
-    public void gerarTabuleiro() {
+    public static void gerarTabuleiro() {
         for (int j = 0; j < 8; j++) for (int i = 0; i < 8; i++) matrizDoTabuleiro[i,j] = "[ ]";        
     }
 
-    public void exibirTabuleiro() {
+    public static void exibirTabuleiro() {
         for (int j = 0; j < 8; j++) {
             for (int i = 0; i < 8; i++) Console.Write(matrizDoTabuleiro[i,j]); Console.WriteLine("");
         }
     }
 
     // GettersAndSetters:
-    public string getEntradaDaMatrizDoTabuleiro(int matrizPosX, int matrizPosY) {
+    public static string getEntradaDaMatrizDoTabuleiro(int matrizPosX, int matrizPosY) {
         return matrizDoTabuleiro[matrizPosX, matrizPosY];
     }
 
-    public void setMatrizDoTabuleiro(int posAntigaX, int posAntigaY, int posAtualX, int posAtualY, string peca) {
+    public static void setMatrizDoTabuleiro(int posAntigaX, int posAntigaY, int posAtualX, int posAtualY, string peca) {
         matrizDoTabuleiro[posAntigaX, posAntigaY] = "[ ]";
         //Console.WriteLine("Apagar Fantasma");
         matrizDoTabuleiro[posAtualX, posAtualY] = "["+peca+"]";
