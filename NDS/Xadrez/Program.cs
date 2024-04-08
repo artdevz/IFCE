ef7de7f22d9b250b542957c6eba6f6d6939627dd
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-08 14:40:16 -0300

    Update Program.cs

--- NDS/Xadrez/Program.cs
+++ NDS/Xadrez/Program.cs
@@ -20,18 +20,18 @@ class Program {
 
         for (int i = 0; i < 16; i++) {
             bool cor =  (i % 2 == 0)? true : false;
-            Peao peao = new Peao(cor, true);
-            peoes.Add(peao);
+            //Peao peao = new Peao(cor, true);
+            //peoes.Add(peao);
         }
         
         for (int i = 0; i < 4; i++) {
             bool cor =  (i % 2 == 0)? true : false;
             Bispo bispo = new Bispo(cor, true);
             bispos.Add(bispo);
-            Cavalo cavalo = new Cavalo(cor, true);
-            cavalos.Add(cavalo);
-            Torre torre = new Torre(cor, true);
-            torres.Add(torre);
+            //Cavalo cavalo = new Cavalo(cor, true);
+            //cavalos.Add(cavalo);
+            //Torre torre = new Torre(cor, true);
+            //torres.Add(torre);
         }        
         
         for (int i = 0; i < 2; i++) {
@@ -43,10 +43,8 @@ class Program {
         }
         //IniciarJogo()}        
         Tabuleiro.exibirTabuleiro();
-        torres[0].MoverTorre(true, 0, 4);  // Irá ser proíbido por haver peões na frente
-        torres[1].MoverTorre(false, 0, 6); // Quando não há peões, a Torre Preta não consegue ultrapassar a Branca por causa da Hitbox        
-        Tabuleiro.exibirTabuleiro();
-        Console.WriteLine(peoes[0].IsBranco);        
+        bispos[0].MoverBispo(true, 1, 2);        
+        Tabuleiro.exibirTabuleiro();                   
     }
 
     public static void IniciarJogo() {
