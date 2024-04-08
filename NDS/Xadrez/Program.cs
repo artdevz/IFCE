b1cf9d6f109e659fcff265d414a93d736a4493e0
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-08 16:12:27 -0300

    Update Program.cs

--- NDS/Xadrez/Program.cs
+++ NDS/Xadrez/Program.cs
@@ -20,18 +20,18 @@ class Program {
 
         for (int i = 0; i < 16; i++) {
             bool cor =  (i % 2 == 0)? true : false;
-            //Peao peao = new Peao(cor, true);
-            //peoes.Add(peao);
+            Peao peao = new Peao(cor, true);
+            peoes.Add(peao);
         }
         
         for (int i = 0; i < 4; i++) {
             bool cor =  (i % 2 == 0)? true : false;
             Bispo bispo = new Bispo(cor, true);
             bispos.Add(bispo);
-            //Cavalo cavalo = new Cavalo(cor, true);
-            //cavalos.Add(cavalo);
-            //Torre torre = new Torre(cor, true);
-            //torres.Add(torre);
+            Cavalo cavalo = new Cavalo(cor, true);
+            cavalos.Add(cavalo);
+            Torre torre = new Torre(cor, true);
+            torres.Add(torre);
         }        
         
         for (int i = 0; i < 2; i++) {
@@ -42,10 +42,12 @@ class Program {
             reis.Add(rei);
         }
         //IniciarJogo()}        
+        Tabuleiro.exibirTabuleiro();        
+        bispos[2].MoverBispo(true, 6, 6);        
         Tabuleiro.exibirTabuleiro();
-        bispos[0].MoverBispo(true, 1, 2);        
-        Tabuleiro.exibirTabuleiro();                   
-    }
+        bispos[2].MoverBispo(true, 0, 0);
+        Tabuleiro.exibirTabuleiro();
+    }        
 
     public static void IniciarJogo() {
         // Ainda fazer
