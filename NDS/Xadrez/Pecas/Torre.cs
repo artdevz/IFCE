aea7ae674f086a33a9335fe1d036522d88d0f393
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-09 14:50:15 -0300

    Update Torre.cs

--- NDS/Xadrez/Pecas/Torre.cs
+++ NDS/Xadrez/Pecas/Torre.cs
@@ -59,19 +59,12 @@ class Torre {
 
     public bool Colisao(int posX, int posY) {     
 
-        if (this.positionX == posX && this.positionY < posY) for (int j = this.positionY; j <= posY; j++) if (Tabuleiro.getEntradaDaMatrizDoTabuleiro(posX, j) != "[ ]") return (fogoAmigo(this.isBranco, posX, posY) == true)? true : false;       
-        if (this.positionX > posX && this.positionY == posY) for (int i = this.positionX; i >= posX; i--) if (Tabuleiro.getEntradaDaMatrizDoTabuleiro(i, posY) != "[ ]") return (fogoAmigo(this.isBranco, posX, posY) == true)? true : false;       
-        if (this.positionX < posX && this.positionY == posY) for (int i = this.positionX; i <= posX; i++) if (Tabuleiro.getEntradaDaMatrizDoTabuleiro(i, posY) != "[ ]") return (fogoAmigo(this.isBranco, posX, posY) == true)? true : false;
-        if (this.positionX == posX && this.positionY > posY) for (int j = this.positionY; j >= posY; j--) if (Tabuleiro.getEntradaDaMatrizDoTabuleiro(posX, j) != "[ ]") return (fogoAmigo(this.isBranco, posX, posY) == true)? true : false;
+        if (this.positionX == posX && this.positionY < posY) for (int j = this.positionY; j <= posY; j++) if (Tabuleiro.getEntradaDaMatrizDoTabuleiro(posX, j) != "[ ]") return (Rodada.fogoAmigo(this.isBranco, posX, posY) == true)? true : false;       
+        if (this.positionX > posX && this.positionY == posY) for (int i = this.positionX; i >= posX; i--) if (Tabuleiro.getEntradaDaMatrizDoTabuleiro(i, posY) != "[ ]") return (Rodada.fogoAmigo(this.isBranco, posX, posY) == true)? true : false;       
+        if (this.positionX < posX && this.positionY == posY) for (int i = this.positionX; i <= posX; i++) if (Tabuleiro.getEntradaDaMatrizDoTabuleiro(i, posY) != "[ ]") return (Rodada.fogoAmigo(this.isBranco, posX, posY) == true)? true : false;
+        if (this.positionX == posX && this.positionY > posY) for (int j = this.positionY; j >= posY; j--) if (Tabuleiro.getEntradaDaMatrizDoTabuleiro(posX, j) != "[ ]") return (Rodada.fogoAmigo(this.isBranco, posX, posY) == true)? true : false;
                
         return false;  
-    }
-
-    public bool fogoAmigo(bool isBranco, int posX, int posY) {
-        string peca = Tabuleiro.getEntradaDaMatrizDoTabuleiro(posX, posY);           
-        if (this.isBranco == true) if (peca == "[\u265d]" || peca == "[\u265e]" || peca == "[\u265f]" || peca == "[\u265b]" || peca == "[\u265a]" || peca == "[\u265c]") return true;        
-        if (this.isBranco == false) if (peca == "[\u2657]" || peca == "[\u2658]" || peca == "[\u2659]" || peca == "[\u2655]" || peca == "[\u2654]" || peca == "[\u2656]") return true;
-        return false;
     }
 
     // GettersAndSetters:
