d63ba87a971c76bacd091388c811a02698978c09
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-10 08:42:24 -0300

    Update Torre.cs

--- NDS/Xadrez/Pecas/Torre.cs
+++ NDS/Xadrez/Pecas/Torre.cs
@@ -10,40 +10,41 @@ class Torre {
     private bool isBranco;
     private int positionX;
     private int positionY;
+    private string icon;
 
     // Construtor:
     public Torre(bool isBranco, bool isInicio) {
         this.isBranco = isBranco;
+        icon = (isBranco)? "\u265c" : "\u2656";
         
         if ((isBranco == true) && (isInicio == true)) {
             positionX = Torre.quantidadeBranco*7;
             positionY = 7;
             Torre.quantidadeBranco++;
-            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, "\u265c");
+            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, icon);
         }
         
         if ((isBranco == false) && (isInicio == true)) {
             positionX = Torre.quantidadePreto*7;
             positionY = 0;
             Torre.quantidadePreto++;
-            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, "\u2656");
+            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, icon);
         }
     }
 
     // Métodos:
-    public void MoverTorre(bool isBranco, int posX, int posY) {
-        // Regra Geral:
-        if ((posX < 0) || (posX > 7) || (posY < 0) || (posY > 7)) Console.WriteLine("[ERRO]: Posição Inválida.");
+    public void moverTorre(bool isBranco, int posX, int posY) {
+        if (Rodada.regraGeral(posX, posY) == false) Console.WriteLine("[ERRO]: Posição Inválida.");
 
         // Regra de Alcance:        
-        if ((MovimentoNoAlcance(posX, posY) == true) && (Colisao(posX, posY) == false)) {                       
+        if ((movimentoNoAlcance(posX, posY) == true) && (colisao(posX, posY) == false)) {                       
             Console.WriteLine("Movimento Permitido.");
             
             // Alterar Tabuleiro:
-            if (isBranco == true)  Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, posX, posY, "\u265c");
-            if (isBranco == false) Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, posX, posY, "\u2656");
+            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, posX, posY, icon);
             this.positionX = posX;
             this.positionY = posY;
+            
         } 
         else {
             Console.WriteLine("Movimento Proíbido.");
@@ -51,13 +52,13 @@ class Torre {
         
     }
     
-    public bool MovimentoNoAlcance(int posX, int posY) {
+    public bool movimentoNoAlcance(int posX, int posY) {
         if ((this.positionX == posX && this.positionY != posY) || (this.positionX != posX && this.positionY == posY)) return true;
         Console.WriteLine("Fora do Alcance");
         return false;
     }
 
-    public bool Colisao(int posX, int posY) {     
+    public bool colisao(int posX, int posY) {     
 
         int caminhoX = (this.positionX < posX)? 1 : -1; 
         int caminhoY = (this.positionY < posY)? 1 : -1;
