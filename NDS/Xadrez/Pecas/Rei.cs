423c74f05e89ccb4b0f374a57e2b12b31d276803
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-10 08:42:09 -0300

    Update Rei.cs

--- NDS/Xadrez/Pecas/Rei.cs
+++ NDS/Xadrez/Pecas/Rei.cs
@@ -10,26 +10,56 @@ class Rei {
     private bool isBranco;
     private int positionX;
     private int positionY;
+    private string icon;
 
     // Construtor:
     public Rei(bool isBranco, bool isInicio) {
         this.isBranco = isBranco;
-        
+        icon = (isBranco)? "\u265a" : "\u2654";
+
         if ((isBranco == true) && (isInicio == true)) {
             positionX = 4;
             positionY = 7;
-            Rei.quantidadeBranco++;
-            //Tabuleiro t = new Tabuleiro();
-            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, "\u265a");
+            Rei.quantidadeBranco++;                      
+            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, icon);
         }
         
         if ((isBranco == false) && (isInicio == true)) {
             positionX = 4;
             positionY = 0;
-            Rei.quantidadePreto++;
-            //Tabuleiro t = new Tabuleiro();
-            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, "\u2654");
+            Rei.quantidadePreto++;            
+            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, icon);
         }
+    }
+
+    // Métodos:
+    public void moverRei(bool isBranco, int posX, int posY) {
+        // Regra Geral:
+        if (Rodada.regraGeral(posX, posY) == false) Console.WriteLine("[ERRO]: Posição Inválida.");
+
+        // Regra de Alcance:
+        if (movimentoNoAlcance(posX, posY) == true) {
+            Console.WriteLine("Movimento Permitido.");
+            
+            // Alterar Tabuleiro:            
+            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, posX, posY, icon);
+            this.positionX = posX;
+            this.positionY = posY;
+
+        } 
+        else {
+            Console.WriteLine("Movimento Proíbido.");
+        }
+    }
+
+    public bool movimentoNoAlcance(int posX, int posY) {     
+        
+        int i = this.positionX; int j = this.positionY;        
+        int caminhoX = (this.positionX == posX)? 0 : ((this.positionX < posX)? 1 : -1);
+        int caminhoY = (this.positionY == posY)? 0 : ((this.positionY < posY)? 1 : -1);     
+          
+        i+=caminhoX; j+=caminhoY;    
+        return (Tabuleiro.getEntradaDaMatrizDoTabuleiro(i, j) == "[ ]")? true : ((Rodada.fogoAmigo(this.isBranco, posX, posY) == true)? false : true);        
     }
 
     // GettersAndSetters:
