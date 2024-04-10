24c5e9bd1499e4a5d407a150ba1f0a06af3063bb
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-10 08:40:46 -0300

    Update Cavalo.cs

--- NDS/Xadrez/Pecas/Cavalo.cs
+++ NDS/Xadrez/Pecas/Cavalo.cs
@@ -10,27 +10,32 @@ class Cavalo {
     private bool isBranco;
     private int positionX;
     private int positionY;
+    private string icon;
 
     // Construtor:
     public Cavalo(bool isBranco, bool isInicio) {
         this.isBranco = isBranco;
+        icon = (isBranco)? "\u265e" : "\u2658"; 
         
         if ((isBranco == true) && (isInicio == true)) {
             positionX = Cavalo.quantidadeBranco + 1 + Cavalo.quantidadePreto*4;
             positionY = 7;
-            Cavalo.quantidadeBranco++;
-            //Tabuleiro t = new Tabuleiro();
-            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, "\u265e");
+            Cavalo.quantidadeBranco++;            
+            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, icon);
         }
         
         if ((isBranco == false) && (isInicio == true)) {
             positionX = Cavalo.quantidadePreto + 1 + Cavalo.quantidadePreto*4;
             positionY = 0;
             Cavalo.quantidadePreto++;
-            //Tabuleiro t = new Tabuleiro();
-            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, "\u2658");
+            
+            Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, this.positionX, this.positionY, icon);
         }
     }
+
+    /*Tabuleiro.setMatrizDoTabuleiro(this.positionX, this.positionY, posX, posY, icon);
+            this.positionX = posX;
+            this.positionY = posY;*/
 
     // GettersAndSetters:
 
