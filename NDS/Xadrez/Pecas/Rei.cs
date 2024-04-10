fcd9c9d1cdf0ba601076335bf66eaa981dab7a83
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2024-04-10 09:08:47 -0300

    Update Rei.cs

--- NDS/Xadrez/Pecas/Rei.cs
+++ NDS/Xadrez/Pecas/Rei.cs
@@ -57,7 +57,7 @@ class Rei {
         int i = this.positionX; int j = this.positionY;        
         int caminhoX = (this.positionX == posX)? 0 : ((this.positionX < posX)? 1 : -1);
         int caminhoY = (this.positionY == posY)? 0 : ((this.positionY < posY)? 1 : -1);     
-          
+        // BUG: Rei está sem limites  
         i+=caminhoX; j+=caminhoY;    
         return (Tabuleiro.getEntradaDaMatrizDoTabuleiro(i, j) == "[ ]")? true : ((Rodada.fogoAmigo(this.isBranco, posX, posY) == true)? false : true);        
     }
